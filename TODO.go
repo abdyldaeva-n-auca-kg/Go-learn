867367b4eace5c5fd14d6e8dcc686bb7d58e5375
Nurperi Abdyldaeva <abdyldaeva_n@auca.kg>
2023-06-28 13:43:03 +0600

    todo list editor

---
+++ TODO.go
@@ -0,0 +1,9 @@
+package main
+
+//This code contains:
+//reading command from stdin
+//turn on program and enter "add my todo"
+//show todo list after show command
+//now todo list is in memory but then turn to keep them in file
+//close todo list and get access only after entering password
+//add editing of todo
